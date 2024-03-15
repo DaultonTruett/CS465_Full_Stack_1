84dfdef11a56e4238ec9fa70bcf8df6c41ed055f
Daulton Truett <dtruett45@gmail.com>
2024-03-15 03:38:35 -0600

    Module 2 - added MVC architecture and started converting base HTML to handlebars templates.

---
+++ app_server/controllers/travel.js
@@ -0,0 +1,12 @@
+
+// GET: travel
+const travel = (req, res) => {
+    res.render('travel', {title: 'Travlr Getaways'});
+};
+
+
+
+
+module.exports = {
+    travel
+}
---
+++ app_server/routes/travel.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const ctrlTravel = require('../controllers/travel');
+
+/* GET home page. */
+router.get('/', ctrlTravel.travel);
+
+
+
+
+module.exports = router;
